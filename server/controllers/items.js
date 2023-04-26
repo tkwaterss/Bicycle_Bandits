31ee5cf051b866999bbca97290706f043734205a
Tobin Waters <tkwaterss@gmail.com>
2023-04-26 14:51:52 -0600

    initial database endpoints complete

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -1,14 +1,23 @@
-const {
-  Product,
-  Labor,
-  TicketLabor,
-  TicketProduct,
-  Ticket,
-} = require("../util/models");
+const { Product, Labor } = require("../util/models");
+const { Op } = require("sequelize");
+const {toTitleCase} = require('../util/formatting')
 
 module.exports = {
+  //recieves query to search both products and labor
+  //TODO Need to figure out reg ex search to ignore letter case
   searchCatelogue: async (req, res) => {
+    const { input } = req.query;
+    let laborInput = input.toUpperCase()
+    let productInput = toTitleCase(input)
     try {
+      const laborResults = await Labor.findAll({
+        where: { laborTitle: { [Op.substring]: laborInput } },
+      });
+      const productResults = await Product.findAll({
+        where: { productTitle: { [Op.substring]: productInput } },
+      });
+      let results = { laborResults, productResults };
+      res.status(200).send(results);
     } catch (err) {
       console.log("error in searchCatelogue");
       console.log(err);
